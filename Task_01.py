be81a055d5285725eb45d66017bb64e2a1bb296d
Andrey <y899yy001>
2022-06-04 22:52:22 +0300

    New fail Task_01

---
+++ Task_01.py
@@ -0,0 +1,47 @@
+
+
+'''
+
+Дан список чисел.
+Создать список в который попадают числа, описывающие возрастающую последовательность и содержащие максимальное количество элементов.
+Пример: [1,5,2,3,4,6,1,7] => [1,2,3,4,6,7]
+[5,2,3,4,,6,1,7] => [2,3,4,6,7]
+Порядок менять нельзя
+
+'''
+
+# N = [1,5,2,3,4,6,1,7]
+
+# def get_up2(nums):
+#     ups = []
+#     minNumber = min(nums)
+#     for i in nums:
+#         if i == minNumber:
+#             ups.append(i)
+#             minNumber += 1
+#         elif i == minNumber + 1:
+#             ups.append(i)
+#             minNumber += 2
+#     return ups
+
+# print(get_up2(N))            
+
+# Создать и заполнить файл случайными целыми значеничми. Выполнить сортировку содержимого файла по возрастанию.
+
+from random import randint
+
+def main(n):
+    with open('RandomNum.txt', 'w+') as ran:
+        for _ in range(n):
+            ran.write(f'{str(randint(1,100))}\n')
+
+N = int(input('Сколько случайных чисел будет храниться в файле? \n'))
+main(N)
+
+with open('RandomNum.txt', 'r') as fr:
+    List_int = fr.readlines()
+    List_int.sort(key=(int))
+
+with open('Sortfile.txt', 'w+') as data:
+    for element in List_int:
+        data.write(element)
