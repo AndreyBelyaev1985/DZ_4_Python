d4a16a8341ba1a8912a82edc49b36bfc3d5936a9
Andrey <y899yy001>
2022-06-05 00:35:28 +0300

    Готовый вариант

--- Task_01.py
+++ Task_01.py
@@ -1,50 +1,64 @@
 
 
-'''
+# '''
 
-Дан список чисел.
-Создать список в который попадают числа, описывающие возрастающую последовательность и содержащие максимальное количество элементов.
-Пример: [1,5,2,3,4,6,1,7] => [1,2,3,4,6,7]
-[5,2,3,4,,6,1,7] => [2,3,4,6,7]
-Порядок менять нельзя
+# Дан список чисел.
+# Создать список в который попадают числа, описывающие возрастающую последовательность и содержащие максимальное количество элементов.
+# Пример: [1,5,2,3,4,6,1,7] => [1,2,3,4,6,7]
+# [5,2,3,4,,6,1,7] => [2,3,4,6,7]
+# Порядок менять нельзя
 
-'''
+# '''
 
-from random import randint
-N = [1, 5, 2, 3, 4, 6, 1, 7]
-
-
-def get_up2(nums):
-    ups = []
-    minNumber = min(nums)
-    for i in nums:
-        if i == minNumber:
-            ups.append(i)
-            minNumber += 1
-        elif i == minNumber + 1:
-            ups.append(i)
-            minNumber += 2
-    return ups
+# from random import randint
+# N = [1, 5, 2, 3, 4, 6, 1, 7]
 
 
-print(get_up2(N))
+# def get_up2(nums):
+#     ups = []
+#     minNumber = min(nums)
+#     for i in nums:
+#         if i == minNumber:
+#             ups.append(i)
+#             minNumber += 1
+#         elif i == minNumber + 1:
+#             ups.append(i)
+#             minNumber += 2
+#     return ups
 
-# Создать и заполнить файл случайными целыми значеничми. Выполнить сортировку содержимого файла по возрастанию.
+
+# print(get_up2(N))
+
+# # Создать и заполнить файл случайными целыми значеничми. Выполнить сортировку содержимого файла по возрастанию.
 
 
-def main(n):
-    with open('RandomNum.txt', 'w+') as ran:
-        for _ in range(n):
-            ran.write(f'{str(randint(1,100))}\n')
+# def main(n):
+#     with open('RandomNum.txt', 'w+') as ran:
+#         for _ in range(n):
+#             ran.write(f'{str(randint(1,100))}\n')
 
 
-N = int(input('Сколько случайных чисел будет храниться в файле? \n'))
-main(N)
+# N = int(input('Сколько случайных чисел будет храниться в файле? \n'))
+# main(N)
 
-with open('RandomNum.txt', 'r') as fr:
-    List_int = fr.readlines()
-    List_int.sort(key=(int))
+# with open('RandomNum.txt', 'r') as fr:
+#     List_int = fr.readlines()
+#     List_int.sort(key=(int))
 
-with open('Sortfile.txt', 'w+') as data:
-    for element in List_int:
-        data.write(element)
+# with open('Sortfile.txt', 'w+') as data:
+#     for element in List_int:
+#         data.write(element)
+
+
+# Найти триплеты и просто выводить их на экран. Триплетом называются три числа, которые в сумме дают .
+# (решение будет долгим, ибо является демонстрационным решением при теме многопоточного программироаания.)
+
+with open('1Kints.txt', 'r') as data:
+    Mylist = data.readlines()
+    Mylist.pop()
+    NumList = list(map(int, Mylist))
+for i in NumList:
+    for j in NumList:
+        for k in NumList:
+            if(i + j + k == 0):
+                print(f'{i} + {j} + {k} = 0')
